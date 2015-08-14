13d7d526f1be6fdaaf50d42715da5f53ea7ec89b
tmaconko <t.maconko@ba.lt>
2015-08-14 15:14:10 +0300

    Fix

--- BalticAmadeus.FluentMdx/CreateNugetPackage.bat
+++ BalticAmadeus.FluentMdx/CreateNugetPackage.bat
@@ -2,5 +2,4 @@ del *.nupkg
 del bin/Release/*.*
 msbuild BalticAmadeus.FluentMdx.csproj /t:Rebuild /property:Configuration=Release
 nuget pack BalticAmadeus.FluentMdx.csproj -Prop Configuration=Release
-nuget setApiKey 7b084f3a-997f-4ef0-ac0a-e0b1b042a09f  
-nuget push *.nupkg
+
